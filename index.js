a46bba9f7e3a4a47d599ae151cd44f86eb1c3e75
durudaniel <danielchiemela16@gmail.com>
2023-09-17 19:53:40 +0100

    Return the largest elements in an array using map

--- index.js
+++ index.js
@@ -770,3 +770,17 @@ function sumAndDifference(absArray,difference){
   },0) - difference
 }
 console.log(sumAndDifference([2,4,5,4,-10],10))
+
+function findLargestNums(largestArray){
+  return largestArray.map((array)=>{
+    //console.log(array)
+    return Math.max(...array)
+  })
+}
+
+console.log(findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]))
+
+console.log(findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]))
+
+console.log(findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]])
+)
